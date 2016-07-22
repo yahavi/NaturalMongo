aa0c49dac341b7ff50339365baedd87cf40f8289
Yahav <yahav.itzhak@mail.huji.ac.il>
2016-07-22 17:49:21 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+/*eslint-env node*/
+
+var co = require('co');
+var MongoUsersDriver = require('./mongo_users_driver');
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+var init = function(){
+    MongoUsersDriver.init();
+    MongoUsersDriver.showRules("restaurants", "alpha");
+    // MongoUsersDriver.grantRule("restaurants", "alpha", ["dbOwner"]);
+    MongoUsersDriver.showRules("restaurants", "alpha");
+};
+
+init();
+
+
+
---
+++ mongo_users_driver.js
@@ -0,0 +1,101 @@
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://159.122.221.134:27017/';
+var co = require('co');
+
+const RULES = ['read', 'readWrite', 'dbAdmin', 'dbOwner', 'userAdmin',
+               'clusterAdmin', 'clusterManager', 'clusterMonitor', 'hostManager',
+               'backup', 'restore', 'readAnyDatabase', 'readWriteAnyDatabase',
+               'userAdminAnyDatabase', 'dbAdminAnyDatabase', 'root'];
+
+
+var dbList = [];
+
+module.exports = {
+
+    init : () => {
+        "use strict";
+        var mongoSession;
+        co(function*() {
+            mongoSession = yield MongoClient.connect(url);
+            console.log("Connected correctly to server");
+            var adminDb = mongoSession.admin();
+
+            // List all the available databases
+            var dbs = yield adminDb.listDatabases();
+
+            for (var dbDetails of yield dbs["databases"]){
+                var currentDb = mongoSession.db(dbDetails["name"]);
+                var collections = [];
+                var dbCollections = yield currentDb.listCollections().toArray();
+
+                for (var collection of yield dbCollections){
+                    collections.push(collection["name"]);
+                }
+
+                dbList.push(new SingleDb(dbDetails["name"], collections));
+
+            }
+
+        }).then(()=>{
+            console.log(dbList);
+            mongoSession.close();
+        }).catch((err)=>{
+            console.error(err.stack);
+            mongoSession.close();
+        });
+
+    },
+
+    showRules : (db, user) => {
+        "use strict";
+        var mongoSession;
+        co(function*() {
+            mongoSession = yield MongoClient.connect(url + db);
+            var res =
+                yield mongoSession.command({usersInfo: {user: user, db: db}});
+            var users = res["users"];
+            var roles = users[0]["roles"];
+            console.log(JSON.stringify(roles))
+        }).then(()=>{
+            mongoSession.close();
+        }).catch((err)=>{
+            console.error(err.stack);
+            mongoSession.close();
+        });
+    },
+
+    grantRule : (db, user, roles) => {
+        "use strict";
+        var mongoSession;
+        co(function*() {
+            mongoSession = yield MongoClient.connect(url + db);
+            yield mongoSession.command({grantRolesToUser: user, roles: roles});
+        }).then(()=>{
+            mongoSession.close();
+        }).catch((err)=>{
+            console.error(err.stack);
+            mongoSession.close();
+        });
+    },
+
+    revokeRule : (db, user, roles) => {
+        "use strict";
+        var mongoSession;
+        co(function*() {
+            mongoSession = yield MongoClient.connect(url + db);
+            yield mongoSession.command({revokeRolesFromUser: user, roles: roles});
+        }).then(()=>{
+            mongoSession.close();
+        }).catch((err)=>{
+            console.error(err.stack);
+            mongoSession.close();
+        });
+    }
+};
+
+var SingleDb = function(name, collections){
+    this.name = name;
+    this.collections = collections;
+};
